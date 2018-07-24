b3b112dfb6a339538d642ac5767d2a3cdd43e90f
Jasper Vercammen <jasper.vercammen@icapps.com>
2018-07-24 10:08:16 +0200

    Use translation file

--- src/scenes/home/Home.view.js
+++ src/scenes/home/Home.view.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './Home.styles';
-import { Colors } from '../../utils';
+import { Colors, Translations } from '../../utils';
 
 class Home extends React.Component {
   login = () => {
@@ -16,22 +16,22 @@ class Home extends React.Component {
     return (
       <View style={styles.container}>
         <View>
-          <Text style={styles.lead}>A simple react native boilerplate with a login action.</Text>
+          <Text style={styles.lead}>{Translations.getLabel('home_description')}</Text>
           <ActivityIndicator animating={this.props.isLoading} size="large" color={Colors.primary} />
           {this.props.token && (
             <View style={styles.success}>
-              <Text style={styles.tokenText}>You are now logged in. Your token: {this.props.token}</Text>
+              <Text style={styles.tokenText}>{Translations.getLabel('home_login_success', { token: this.props.token })}</Text>
             </View>
           )}
           {this.props.serverError && (
             <View style={styles.error}>
-              <Text style={styles.tokenText}>There went something wrong: {this.props.serverError}</Text>
+              <Text style={styles.tokenText}>{Translations.getLabel('home_login_error', { token: this.props.serverError })}</Text>
             </View>
           )}
 
         </View>
         <TouchableOpacity style={styles.button} onPress={this.login}>
-          <Text style={styles.buttonText}>Fire a login action!</Text>
+          <Text style={styles.buttonText}>{Translations.getLabel('home_action')}</Text>
         </TouchableOpacity>
       </View>
     );
