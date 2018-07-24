2de2ffecb69416ac8e451f0e1c1814b1903e1b70
Jasper Vercammen <jasper.vercammen@icapps.com>
2018-07-24 09:13:18 +0200

    Added React native config

---
+++ ios/build/Build/Intermediates.noindex/ReactNativeConfig.build/Debug-iphonesimulator/ReactNativeConfig.build/Objects-normal/x86_64/ReactNativeConfig.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/jasper/iCapps/reactnative_boilerplate/node_modules/react-native-config/ios/ReactNativeConfig/ReactNativeConfig.m \
+  /Users/jasper/iCapps/reactnative_boilerplate/node_modules/react-native-config/ios/ReactNativeConfig/ReactNativeConfig.h \
+  /Users/jasper/iCapps/reactnative_boilerplate/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/jasper/iCapps/reactnative_boilerplate/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/jasper/iCapps/reactnative_boilerplate/ios/build/Build/Products/GeneratedDotEnv.m
---
+++ ios/build/Build/Products/GeneratedDotEnv.m
@@ -0,0 +1,1 @@
+  #define DOT_ENV @{ @"IS_DEBUG":@"true",@"BASE_API_URL":@"https://reqres.in/api/" };
