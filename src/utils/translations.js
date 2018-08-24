0db3fbca0fa1ceee2963f804d822830249f44b37
Maarten Anckaert <maarten.anckaert@icapps.com>
2018-08-24 15:20:57 +0200

    Typo

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -9,7 +9,7 @@ const getLabel = (key, inserts = {}) => I18n.t(key, inserts);
 // Export specific function to get a label
 // If the key is part of the label and not starting from index 0, we know it's a label that isn't found
 // Since labels that are not found are formatted as [missing "<lang>.<key>" translation] not found
-const doesLabelExists = key => getLabel(key).indexOf(key) <= 0;
+const doesLabelExist = key => getLabel(key).indexOf(key) <= 0;
 
 const getDeviceLanguage = () => {
   if (DeviceInfo.getDeviceLocale().startsWith('nl')) return 'nl';
@@ -30,7 +30,7 @@ const translations = {
 I18n.translations = translations;
 
 export default {
-  doesLabelExists,
+  doesLabelExist,
   getDeviceLanguage,
   getLabel,
   I18n,
